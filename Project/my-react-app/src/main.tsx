5a42bfb17078bdebde0dd127d7fb6db9f67a70fa
Hritick Bhushan <jobs4hritick@gmail.com>
2024-09-24 00:11:30 +0530

    Added react-router starter code

--- Project/my-react-app/src/main.tsx
+++ Project/my-react-app/src/main.tsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom"
+
+//import App from './App.tsx'
+
 import './index.css'
+import Root from "./routes/root.tsx"
+import ErrorPage from "./error-page.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
---
+++ Project/my-react-app/src/routes/root.tsx
@@ -0,0 +1,11 @@
+function Root () {
+    return(
+        <>
+        <div className="sidebar">
+            <h1> </h1>
+        </div>
+        </>
+    )
+}
+
+export default Root
